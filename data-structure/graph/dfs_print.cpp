66cf5687039c518bbf534a07333c0bde9ce175ec
Pratush26 <pratushbarua26@gmail.com>
2026-02-13 22:51:23 +0600

    added graph dfs travarsal

---
+++ data-structure/graph/dfs_print.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dfs(int src, vector<int> arr[], vector<bool> &visited) {
+    cout << src << " ";
+    visited[src] = true;
+    for(int i: arr[src]) if (!visited[i]) dfs(i, arr, visited);
+}
+
+int main() {
+    int n, e, r, c, check;
+    cin >> n >> e;
+    vector<int> arr[n];
+    vector<bool> visited(n, false);
+    while (e--) {
+        cin >> r >> c;
+        arr[r].push_back(c);
+        arr[c].push_back(r);
+    }
+    dfs(0, arr, visited);
+
+    return 0;
+}
+
+//  sample input
+// 7 5
+// 0 1
+// 1 3
+// 3 2
+// 4 6
+// 3 5
