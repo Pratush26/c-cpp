af381977a72ad07fa7b688636158de0f4f9a6895
Pratush26 <pratushbarua26@gmail.com>
2026-04-12 14:46:06 +0600

    updated map

--- data-structure/map/basic.cpp
+++ data-structure/map/basic.cpp
@@ -8,5 +8,14 @@ int main() {
     mp["rakib"] = 43;
     
     for (auto i = mp.begin(); i != mp.end(); i++) cout << "Name: " << i->first << ", Roll: " << i->second << "\n";
+    // for(auto [key, value]: mp) cout << "Name: " << key << ", Roll: " << value << "\n";
+    
+    // safety check
+    mp.erase("sakib");
+    
+    auto it = mp.find("sakib");
+    if(it == mp.end()) cout << "Key not found";
+    else cout << it->first << " -> " << it->second;
+    
     return 0;
 }
